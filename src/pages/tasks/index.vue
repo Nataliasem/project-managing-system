3e1c7a1ab91d38245d1f8607848f7fcd27bcd155
natalia_semka <seraya.93@inbox.ru>
2025-11-04 19:01:47 +0800

    add TasksTable

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, h } from 'vue'
 import { supabase } from '@/database/supabaseClient'
-import type { Tables } from '../../../database.types'
+import type { Tables } from '../../../database/types'
+import type { ColumnDef } from '@tanstack/vue-table'
+import DataTable from '@/components/ui/table/data-table/DataTable.vue'
 
-const tasks = ref<Tables<'tasks'>[] | null>([])
+const tasks = ref<Tables<'tasks'>[] | null>(null)
 
 ;(async () => {
   const { data, error } = await supabase.from('tasks').select()
@@ -11,22 +13,54 @@ const tasks = ref<Tables<'tasks'>[] | null>([])
     console.error(error)
   }
   tasks.value = data
-
-  console.log(tasks)
 })()
+
+
+const columns: ColumnDef<Tables<'tasks'>>[] = [
+  {
+    accessorKey: 'name',
+    header: () => h('div', { class: 'text-left' }, 'Name'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('name'))
+    }
+  },
+  {
+    accessorKey: 'status',
+    header: () => h('div', { class: 'text-left' }, 'Status'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+    }
+  },
+  {
+    accessorKey: 'due_date',
+    header: () => h('div', { class: 'text-left' }, 'Due Date'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('due_date'))
+    }
+  },
+  {
+    accessorKey: 'project_id',
+    header: () => h('div', { class: 'text-left' }, 'Project'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('project_id'))
+    }
+  },
+  {
+    accessorKey: 'collaborators',
+    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
+    cell: ({ row }) => {
+      return h(
+        'div',
+        { class: 'text-left font-medium' },
+        JSON.stringify(row.getValue('collaborators'))
+      )
+    }
+  }
+]
 </script>
 
 <template>
-  <div>
-    <h1>Tasks Page</h1>
-    <RouterLink to="/">Go to home</RouterLink>
-    <ul>
-      <li v-for="item in tasks" :key="item.id">
-        {{ item.name }}
-        {{ item.slug }}
-      </li>
-    </ul>
-  </div>
+  <DataTable v-if="tasks" :columns="columns" :data="tasks" />
 </template>
 
 <style scoped></style>
