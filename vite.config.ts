4481b71aa946d893d75033789f4b39c5b1eec3c0
natalia_semka <seraya.93@inbox.ru>
2025-11-11 11:40:10 +0300

    add unplugin-auto-import/vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,27 @@ import VueRouter from 'unplugin-vue-router/vite'
 import Tailwindcss from '@tailwindcss/vite'
 import Vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     VueRouter(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/,
+        /\.vue$/, /\.vue\?vue/,
+        /\.md$/, //
+      ],
+
+      imports: [
+        'vue',
+        'vue-router'
+      ],
+      dts: true,
+      viteOptimizeDeps: true
+    }),
     Vue({
       template: {
         compilerOptions: {
